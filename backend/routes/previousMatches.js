2383c2a19e94c457b37bb846cf277e01596f4c80
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2024-05-07 18:19:34 +0530

    Running and changes at 5:30

--- backend/routes/previousMatches.js
+++ backend/routes/previousMatches.js
@@ -6,8 +6,23 @@ const Match = require('../model/match');
 router.get('/', async (req, res) => {
   try {
     const today = new Date();
+
+//     // Get current date and time
+// // const today = new Date();
+
+// // Get current time zone offset in minutes
+// const offsetMinutes = today.getTimezoneOffset();
+
+// // Indian Standard Time (IST) offset is UTC+5:30
+// const istOffset = 5 * 60 + 30; // Convert hours to minutes and add 30 minutes
+
+// // Add the offset for IST
+// const indianTime = new Date(today.getTime() + (offsetMinutes + istOffset) * 60000);
+
+// console.log("Current Indian Time:", indianTime);
+
     console.log("today : ", today)
-    // today.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
     console.log("after today: ", today)
     const previousMatches = await Match.find({ matchDate: { $lt: today } }).sort({ matchDate: -1 });
     res.send(previousMatches);
