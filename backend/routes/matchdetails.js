3f5b80273324de5d0dbe5c8d3f937bfb0c0e2b14
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2024-05-15 01:30:22 +0530

    match details back again old

--- backend/routes/matchdetails.js
+++ backend/routes/matchdetails.js
@@ -11,26 +11,26 @@ router.get('/:uniqueId', async (req, res) => {
       console.log(uniqueId)
 
        // Define an array of collections to search
-       const collections = [Match, Bigbash]; // Assuming Match and Bigbash are Mongoose models
+      //  const collections = [Match, Bigbash]; // Assuming Match and Bigbash are Mongoose models
 
-       let match = null;
+      //  let match = null;
        
-       // Loop through each model and try to find the document
-       for (let i = 0; i < collections.length; i++) {
-           const Model = collections[i]; // Get the Mongoose model from the array
-           console.log(Model);
+      //  // Loop through each model and try to find the document
+      //  for (let i = 0; i < collections.length; i++) {
+      //      const Model = collections[i]; // Get the Mongoose model from the array
+      //      console.log(Model);
            
-           try {
-               match = await Model.findOne(uniqueId); // Find the document using the model directly
+      //      try {
+      //          match = await Model.findOne(uniqueId); // Find the document using the model directly
                
-               if (match) {
-                   break; // Exit loop if a match is found
-               }
-           } catch (err) {
-               console.error(err); // Log any errors
-           }
-       }
-      // const match = await Match.findOne(uniqueId)
+      //          if (match) {
+      //              break; // Exit loop if a match is found
+      //          }
+      //      } catch (err) {
+      //          console.error(err); // Log any errors
+      //      }
+      //  }
+      const match = await Match.findOne(uniqueId)
       
       if (match) {
         res.json(match);
