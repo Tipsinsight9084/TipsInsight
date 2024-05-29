eedd6a6253f2e8dae0b96987ee073d25fa990668
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2024-05-30 01:44:55 +0530

    WC24

--- backend/routes/create.js
+++ backend/routes/create.js
@@ -131,6 +131,48 @@ router.post('/WC23', async (req, res) => {
   }
 });
 
+router.post('/WC24', async (req, res) => {
+  try {
+    const { team1, team2, matchDate, time, venue, tossWinner, tossDate, tossTime, matchWinner, matchwinnerDate, matchwinnerTime, uniqueId, team1Run , team1Wicket, team1Over, team2Run , team2Wicket, team2Over, actualmatchWinner, actualtossWinner, highlight, series, year} = req.body;
+
+    const newWC24 = new WC24({
+      team1,
+      team2,
+      matchDate,
+      time,
+      venue,
+      tossWinner,
+      tossDate,
+      tossTime,
+      matchWinner,
+      matchwinnerDate,
+      matchwinnerTime,
+      uniqueId,
+      team1Run ,
+      team1Wicket,
+      team1Over, 
+      team2Run , 
+      team2Wicket, 
+      team2Over,
+      actualmatchWinner,
+      actualtossWinner,
+      highlight,
+      series,
+      year,
+    });
+
+    console.log(req.body)
+    console.log("QI")
+    console.log("WC24",newWC24)
+    // const 
+    await newWC24.save();
+
+    res.status(201).json({ message: 'WC24 Match created successfully.' });
+  } catch (err) {
+    res.status(400).json({ message: err + 'error in' });
+  }
+});
+
 router.post('/SA24', async (req, res) => {
   try {
     const { team1, team2, matchDate, time, venue, tossWinner, tossDate, tossTime, matchWinner, matchwinnerDate, matchwinnerTime, uniqueId, team1Run , team1Wicket, team1Over, team2Run , team2Wicket, team2Over, actualmatchWinner, actualtossWinner, highlight, series, year} = req.body;
@@ -163,11 +205,11 @@ router.post('/SA24', async (req, res) => {
 
     console.log(req.body)
     console.log("QI")
-    console.log("bigbash",newSA24)
+    console.log("SA24",newSA24)
     // const 
     await newSA24.save();
 
-    res.status(201).json({ message: 'Bigbash Match created successfully.' });
+    res.status(201).json({ message: 'SA24 Match created successfully.' });
   } catch (err) {
     res.status(400).json({ message: err + 'error in' });
   }
@@ -205,11 +247,11 @@ router.post('/ENGvPAK24', async (req, res) => {
 
     console.log(req.body)
     console.log("QI")
-    console.log("bigbash",newENGvPAK24)
+    console.log("ENGvPAK24",newENGvPAK24)
     // const 
     await newENGvPAK24.save();
 
-    res.status(201).json({ message: 'Bigbash Match created successfully.' });
+    res.status(201).json({ message: 'ENGvPAK24 Match created successfully.' });
   } catch (err) {
     res.status(400).json({ message: err + 'error in' });
   }
@@ -247,11 +289,11 @@ router.post('/WIvSA24', async (req, res) => {
 
     console.log(req.body)
     console.log("QI")
-    console.log("bigbash",newWIvSA24)
+    console.log("WIvSA24",newWIvSA24)
     // const 
     await newWIvSA24.save();
 
-    res.status(201).json({ message: 'Bigbash Match created successfully.' });
+    res.status(201).json({ message: 'WIvSA24 Match created successfully.' });
   } catch (err) {
     res.status(400).json({ message: err + 'error in' });
   }
--- backend/routes/upcomingMatches.js
+++ backend/routes/upcomingMatches.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Match = require('../model/match');
+const WC24 = require('../model/WC24');
 const moment = require('moment-timezone');
 
 
@@ -12,6 +13,20 @@ router.get('/', async (req, res) => {
       // today.setHours(0, 0, 0, 0);
       console.log(today)
       const upcomingMatches = await Match.find({ matchDate: { $gte: today.format() } }).sort({ matchDate: 1 });
+      res.json(upcomingMatches);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+router.get('/WC24', async (req, res) => {
+  try {
+      const today = moment().tz('Asia/Kolkata');
+      // const today = new Date();
+      console.log(today)
+      // today.setHours(0, 0, 0, 0);
+      console.log(today)
+      const upcomingMatches = await WC24.find({ matchDate: { $gte: today.format() } }).sort({ matchDate: 1 });
       res.json(upcomingMatches);
     } catch (err) {
       res.status(500).json({ message: err.message });
