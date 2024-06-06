dbfa8683648b9a59d7c029ca316f5544dc6b1697
Shivangi <sshivangi332@gmail.com>
2024-06-07 01:05:49 +0530

    create teams

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 // const teams = ["CSK", "GT", "DC", "KKR", "LSG", "MI", "PBKS", "RR", "RCB", "SRH","SIX","HUR","HEA","SCO","STR","STA","REN","THU"];
 // const teams = ["SIX","HUR","HEA","SCO","STR","STA","REN","THU"];
 // const teams = ["SEC" , "DSG" , "PR" , "JSK" , "PC" , "MICT"];
-const teams = ["PAP" , "UGA" , "PAK" , "OMA" , "AUS" , "NAM" , "SCOT" , "USA", "CAN" , "IRE", "IND"];
+// const teams = ["PAP" , "UGA" , "PAK" , "OMA" , "AUS" , "NAM" , "SCOT" , "USA", "CAN" , "IRE", "IND"];
 // const teams = ["WI",'SA']
 // const teams =[ "IND","SA",'AUS','PAK','NED','ENG','NZ','BAN','SL','AFG']
 // const teams = ["PAK",'ENG']
